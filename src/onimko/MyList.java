a5975c008adf52f73958247e5e0faec1648b65e2
shurick2211@gmail.com <Dfvgbhs2609>
2022-09-23 18:17:36 +0300

    stream API+

--- src/onimko/MyList.java
+++ src/onimko/MyList.java
@@ -19,13 +19,25 @@ public interface MyList<E> {
   void forEach(Consumer<? super E> consumer);
   Object[] toArray();
 
+  /**
+   * Method gives opportunity to use the MyList for Stream API.
+   * @return Stream<E> for use.
+   */
   default Stream<E> stream() { return (Stream<E>) Arrays.stream(this.toArray()); }
 
-  default void addAll(Collection<E> collection) {
+  /**
+   * Method adds another collection on the MyList.
+   * @param collection the input collection.
+   */
+  default void addAll(Collection<? extends E> collection) {
     collection.forEach(this::add);
   }
 
-  default void removeAll(Collection<E> collection) {
+  /**
+   * Method removes another collection without the MyList.
+   * @param collection the input collection.
+   */
+  default void removeAll(Collection<? extends E> collection) {
     collection.forEach(this::remove);
   }
 }
